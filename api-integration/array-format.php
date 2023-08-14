2ea7b346a1349325c251d3eed57b6c373edc38ca
Ahsan_4 <84592484+ahsanjawed4@users.noreply.github.com>
2023-08-14 21:30:21 +0500

    ...

--- api-integration/array-format.php
+++ api-integration/array-format.php
@@ -12,7 +12,7 @@
         curl_setopt_array($curl_array, [
             CURLOPT_URL => $URL,
             CURLOPT_RETURNTRANSFER => true,
-            // CURLOPT_ENCODING => "",
+            // CURLOPT_ENCODING => "",ahsan jawed remeber the name
             // CURLOPT_MAXREDIRS => 10,
             // CURLOPT_TIMEOUT => 30,
             // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
